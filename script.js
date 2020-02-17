2aacf4406ce974797fefb8ad88f755560747e01e
viviRbi <50475727+viviRbi@users.noreply.github.com>
2020-02-16 21:55:33 -0600

    update

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-
 var pass = document.querySelector('input')
-
-console.log(pass)
 
 var first =0;
 
 
 document.querySelector('.button').addEventListener('click', ask)
 
-
+let re = localStorage.getItem("cook")||0
+if (re === "ray"){
+		document.querySelector('section').style.display="none"
+		document.querySelector('main').style.display="block"
+}
 
 function ask(){
-	
-
 	if (pass.value.toLowerCase() === "ray"){
+		const re ="ray"
+		localStorage.setItem("cook", re)
+		document.querySelector('section').style.display="none"
+		document.querySelector('main').style.display="block"
+	}
+	else if (localStorage.getItem("cook") === "ray"){
 		document.querySelector('section').style.display="none"
 		document.querySelector('main').style.display="block"
 	}
 	else{
 		pass.value = "";
-		document.querySelector('section h6').innerText= "Sorry. Wrong answer."
+		document.querySelector('section h6').innerText= "Hint: It's end with letter y."
 	}
 }
-
-
-
